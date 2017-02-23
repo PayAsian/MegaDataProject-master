5172bd9f7eb476d2bbfea590033e80c80d66df67
PayAsian <PayAsian@yahoo.com>
2017-02-23 13:02:04 -0700

    adding code to addAtIndex

--- MegaData/Model/List.h
+++ MegaData/Model/List.h
@@ -17,8 +17,77 @@ private:
     Node<Type> * front;
     
 public:
-    
+    List();
+    List(int size);
+    List<Type>();
+    List<Type>(const List<Type> & toBeCopied);
+    ~List<Type>();
+    void setAtIndex(int index, Type value);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    int getSize();
     
 };
+
+template<class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->setNodePointer(front);
+        
+        this->front = newFirst;
+    }
+    
+    size++;
+}
+
+template<class Type>
+void List<Type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this->front = added;
+        this->end = added;
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    
+    size++;
+}
+
+template<class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index <= size);
+    if(index == 0)
+    {
+        addFront(value);
+    }
+    else if
+    {
+        addEnd(value);
+    }
+    else
+    {
+        Node<Type> * insertedNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+    }
+    
+}
+
 
 #endif /* List_h */
