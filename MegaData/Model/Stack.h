0905a48e66deb714c067fbefd5e078fb408197a4
PayAsian <PayAsian@yahoo.com>
2017-03-07 13:25:03 -0700

    added stack

---
+++ MegaData/Model/Stack.h
@@ -0,0 +1,98 @@
+//
+//  Stack.h
+//  MegaData
+//
+//  Created by Trevino, Peyton on 3/7/17.
+//  Copyright © 2017 Trevino, Peyton. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+
+
+template<class Type>
+class Stack : public DoublyLinkedList<Type>
+{
+private:
+public:
+    Stack();
+    ~Stack();
+    void add(Type value);
+    Type remove(int index);
+    Type pop();
+    Type peek();
+    void push(Type data);
+}
+
+template<class Type>
+Stack<Type> :: Stack() : DoublyLinkedList<Type>()
+{
+    
+}
+
+template<class Type>
+Stack<Type> :: ~Stack()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove = this->getFront();
+    }
+
+}
+
+template<class Type>
+void Stack<Type> :: add(Type valueToAdd)
+{
+    push(valueToAdd);
+}
+
+template<class Type>
+void Stack<Type> :: push(Type addedThing)
+{
+    BiDirecitonalNode<Type> * addToStack = new BiDirectionalnNode<Type>(addedThing);
+    
+    if(this->getSize() == 0 || this->getFront() == nullptr || this->getEnd() == nullptr)
+    {
+        this->setFront(addToStack);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addToStack);
+        addToStack->setPreviousPointer(this->getEnd());
+    }
+    this->setEnd(addToStack);
+    this->setSize(this->getSize() +1 );
+}
+
+template<class Type>
+Type Stack<Type> :: remove(int index)
+{
+    assert(index == this->getSize() - 1 && this->getSize() > 0);
+    return pop();
+}
+
+template<class Type>
+Type Stack<Type> :: peek()
+{
+    assert(this->getSize() >0);
+    Type removed = this->getEnd()->getNodeData();
+    
+    BiDirectionalNode<Type> * update = this->getEnd();
+    update = update->getPreviousPointer();
+    
+    if(update != nullptr)
+    {
+        update->setNextPointer(nullptr);
+    }
+    
+    delete this->getEnd();
+    
+    this->setEnd(update);
+}
+
+
+#endif /* Stack_h */
